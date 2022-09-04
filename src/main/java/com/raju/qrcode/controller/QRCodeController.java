84b3dc61f58e0745b1cbda3e0fa9cdb46bc9e5e8
raju <raju@istlbs.com>
2022-09-04 17:58:56 +0600

    first commit

---
+++ src/main/java/com/raju/qrcode/controller/QRCodeController.java
@@ -0,0 +1,27 @@
+package com.raju.qrcode.controller;
+
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
+@RequestMapping("/qr-code")
+public class QRCodeController {
+
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView getQrGeneratePage(Model model, HttpServletRequest request, HttpServletResponse response) {
+
+
+        return new ModelAndView("");
+
+    }
+
+
+}
